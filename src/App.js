2a148b6669db51df00aba83d5a0a80f5acf187e2
Manir Uddin <manir22sust@gmail.com>
2023-01-24 11:39:18 +0100

    using redux-toolkit fetch data for posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { CounterView } from "./features/counter/CounterView";
+import { PostsView } from "./features/posts/PostsView";
 
 function App() {
   return (
     <div className="App">
       <h1> Counter App</h1>
       <CounterView />
+      <h2> List of posts</h2>
+      <PostsView />
     </div>
   );
 }
